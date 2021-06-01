f3522e771c2c0aad40875b0fdcfc4610d8186b91
Alec Caputo <acaputo@mail.smcvt.edu>
2021-06-01 13:44:50 -0600

    initial commit

---
+++ index.ts
@@ -0,0 +1,77 @@
+import * as Discord from 'discord.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const serverId = process.env.SERVER;
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('ready');
+});
+
+client.login(process.env.TOKEN);
+
+client.on('message', async (message) => {
+    // don't respond to messages from self
+    if (message.author.id === client.user?.id) {
+        return;
+    }
+    if (message.channel.type === "dm") {
+        const server = client.guilds.cache.find(guild => guild.id === serverId);
+        if (!server) {
+            await message.channel.send("Looks like you're not in the server.")
+            return;
+        }
+        const content = message.content;
+        if (content.toLocaleLowerCase() === 'yes')
+        {
+            const guildMember = server.member(message.author);
+            if (process.env.INTRO_ROLE_ID) {
+                guildMember?.roles.add(process.env.INTRO_ROLE_ID)
+                await message.channel.send("Great, you're all set!")
+            }
+            return;
+        } else if (content.toLocaleLowerCase() === 'no') {
+            const guildMember = server.member(message.author);
+            if (process.env.NOT_IN_CLAN_ROLE_ID) {
+                guildMember?.roles.add(process.env.NOT_IN_CLAN_ROLE_ID)
+                // message the mods to inform them
+                const mods = server.members.cache.filter(member => member.roles.cache.some(r => r.id === process.env.MOD_ROLE_ID));
+                mods.forEach(mod => mod.send(`Please add <@${message.author.id}> to the in game clan`));
+                await message.channel.send("No problem, we've messaged the mods and will get you in ASAP")
+                return;
+            }
+        } else {
+            const splitMessage = content.length ? content.split(' ') : [];
+            if (splitMessage.length !== 2 || !splitMessage[0]?.startsWith('!') || !splitMessage[1].length) {
+                await message.channel.send("Looks like there was a problem with your message. Make sure it is two words, the first being !ign followed by you Oldschool RuneScape name")
+                return;
+            }
+            if (splitMessage[0].toLocaleLowerCase() === '!ign') {
+                const server = client.guilds.cache.find(guild => guild.id === serverId)
+                if (server) {
+                    try {
+                        await server.member(message.author)?.setNickname(splitMessage[1]);
+                        await message.channel.send("Great! Your name is now set in the discord server!\n Have you been accepted into the in-game clan system?")
+                    } catch (e) {
+                        await message.channel.send("Hmmm, something went wrong, please try again.")
+                    }
+                }
+            }
+        }
+    }
+});
+
+client.on('guildMemberAdd', async (member) => {
+    const welcomeChannel = client.channels.cache.get(process.env.WELCOME_CHANNEL_ID ?? '');
+    if (welcomeChannel && welcomeChannel.isText()) {
+        try {
+            await welcomeChannel.send(`Welcome <@${member.id}>! Keep an eye out for a PM to complete your registration!`);
+        } catch (e) {
+            // still attempt to PM the user if we weren't able to send the message to the channel
+            console.warn("Unable to send welcome message to welcome channel...attempting to send PM")
+        }
+    }
+    await member.send("Welcome to the clan! Reply !ign [Your In Game Name]\n For example `!ign Thugga`");
+})
