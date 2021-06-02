ad542ab97519f36d2a984bea96839b96b434e6f0
Alec Caputo <acaputo@mail.smcvt.edu>
2021-06-01 21:02:44 -0600

    allow multi word igns

--- index.ts
+++ index.ts
@@ -22,8 +22,8 @@ client.on('message', async (message) => {
             await message.channel.send("Looks like you're not in the server.")
             return;
         }
-        const content = message.content;
-        if (content.toLocaleLowerCase() === 'yes')
+        const content = message.content.toLocaleLowerCase();
+        if (content === 'yes')
         {
             const guildMember = server.member(message.author);
             if (process.env.INTRO_ROLE_ID) {
@@ -31,7 +31,7 @@ client.on('message', async (message) => {
                 await message.channel.send("Great, you're all set!")
             }
             return;
-        } else if (content.toLocaleLowerCase() === 'no') {
+        } else if (content === 'no') {
             const guildMember = server.member(message.author);
             if (process.env.NOT_IN_CLAN_ROLE_ID) {
                 guildMember?.roles.add(process.env.NOT_IN_CLAN_ROLE_ID)
@@ -43,16 +43,17 @@ client.on('message', async (message) => {
             }
         } else {
             const splitMessage = content.length ? content.split(' ') : [];
-            if (splitMessage.length !== 2 || !splitMessage[0]?.startsWith('!') || !splitMessage[1].length) {
-                await message.channel.send("Looks like there was a problem with your message. Make sure it is two words, the first being !ign followed by you Oldschool RuneScape name")
+            if (splitMessage.length < 2 || !splitMessage[0]?.startsWith('!')) {
+                await message.channel.send("Looks like there was a problem with your message. Make sure it is at least 2 words, the first being !ign followed by you Oldschool RuneScape name")
                 return;
             }
             if (splitMessage[0].toLocaleLowerCase() === '!ign') {
                 const server = client.guilds.cache.find(guild => guild.id === serverId)
                 if (server) {
                     try {
-                        await server.member(message.author)?.setNickname(splitMessage[1]);
-                        await message.channel.send("Great! Your name is now set in the discord server!\n Have you been accepted into the in-game clan system?")
+                        const username = splitMessage.slice(0, 1).join(" ");
+                        await server.member(message.author)?.setNickname(username);
+                        await message.channel.send("Great! Your name is now set in the discord server!\n Have you been accepted into the in-game clan system? Reply yes or no.")
                     } catch (e) {
                         await message.channel.send("Hmmm, something went wrong, please try again.")
                     }
@@ -66,7 +67,7 @@ client.on('guildMemberAdd', async (member) => {
     const welcomeChannel = client.channels.cache.get(process.env.WELCOME_CHANNEL_ID ?? '');
     if (welcomeChannel && welcomeChannel.isText()) {
         try {
-            await welcomeChannel.send(`Welcome <@${member.id}>! Keep an eye out for a PM to complete your registration!`);
+            await welcomeChannel.send(`Welcome <@${member.id}>! Keep an eye out for a DM to complete your registration!`);
         } catch (e) {
             // still attempt to PM the user if we weren't able to send the message to the channel
             console.warn("Unable to send welcome message to welcome channel...attempting to send PM")
