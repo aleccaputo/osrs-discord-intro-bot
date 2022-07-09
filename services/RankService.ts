25f19722fe0a3efa0b36703db41e0b7ec81beb90
Alec Caputo <acaputo@mail.smcvt.edu>
2022-07-09 17:48:58 -0600

    typo

--- services/RankService.ts
+++ services/RankService.ts
@@ -5,7 +5,7 @@ import {formatDiscordUserTag} from "./MessageHelpers";
 export const createPointsLeaderboard = async () => {
     const users = await getUsersByPointsDesc();
     const topTenUsers = users.slice(0, 10);
-    const embedUsers: EmbedField[] = topTenUsers.map((x, idx) => ({name: idx + 1, value: `${formatDiscordUserTag(x.discordId)}: ${x.points.toString(10)}`, inline: false}));
+    const embedUsers: EmbedField[] = topTenUsers.map((x, idx) => ({name: (idx + 1).toString(10), value: `${formatDiscordUserTag(x.discordId)}: ${x.points}`, inline: false}));
     return {
         color: 0x8b0000,
         title: 'ChillTopia Leaderboard',
