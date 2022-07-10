81b6f3afb0705ec1559647ad09c8f187eedc9916
Alec Caputo <acaputo@mail.smcvt.edu>
2022-07-09 18:24:17 -0600

    double newline and +1 on idx

--- services/RankService.ts
+++ services/RankService.ts
@@ -6,9 +6,8 @@ import {convertNumberToEmoji} from "./DropSubmissionService";
 export const createPointsLeaderboard = async () => {
     const users = await getUsersByPointsDesc();
     const topTenUsers = users.slice(0, 10);
-    const embedUsers: EmbedField[] = topTenUsers.map((x, idx) => ({name: 'Top 10', value: `${formatDiscordUserTag(x.discordId)}: ${x.points}`, inline: false}));
-    const formatted = topTenUsers.map((x, idx) => `${convertNumberToEmoji(idx)} ${formatDiscordUserTag(x.discordId)}: ${x.points} points`)
-    const test: EmbedField = {name: 'Top 10', value: formatted.join('\r\n'), inline: false};
+    const formatted = topTenUsers.map((x, idx) => `${convertNumberToEmoji(idx + 1)} ${formatDiscordUserTag(x.discordId)}: ${x.points} points`)
+    const test: EmbedField = {name: 'Top 10', value: formatted.join('\r\n\r\n'), inline: false};
 
     return {
         color: 0x8b0000,
