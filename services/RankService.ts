c8dee73fb76371729bda5ebb536ae18787a65e23
Alec Caputo <acaputo@mail.smcvt.edu>
2022-07-14 20:56:32 -0600

    Update RankService.ts

--- services/RankService.ts
+++ services/RankService.ts
@@ -5,9 +5,9 @@ import {convertNumberToEmoji} from "./DropSubmissionService";
 
 export const createPointsLeaderboard = async () => {
     const users = await getUsersByPointsDesc();
-    const topTenUsers = users.slice(0, 50);
+    const topTenUsers = users.slice(0, 20);
     const formatted = topTenUsers.map((x, idx) => `${convertNumberToEmoji(idx + 1)} ${formatDiscordUserTag(x.discordId)}: ${x.points} points`)
-    const test: EmbedField = {name: 'Top 50', value: formatted.join('\r\n\r\n'), inline: false};
+    const test: EmbedField = {name: 'Top 20', value: formatted.join('\r\n\r\n'), inline: false};
 
     return {
         color: 0x8b0000,
