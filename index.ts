9ffb377b6b0e261b9da0df9ea98c31541a719549
Alec Caputo <acaputo@mail.smcvt.edu>
2023-01-19 14:01:43 -0700

    Starting the transition to slash commands

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as Discord from 'discord.js';
-import {ChannelType, GatewayIntentBits, Partials, User} from 'discord.js';
+import {ChannelType, Collection, Events, GatewayIntentBits, Partials, User} from 'discord.js';
 import * as dotenv from 'dotenv';
 import {
     initializeReportMembersEligibleForPointsBasedRankUp,
@@ -32,6 +32,8 @@ import {createPointsLeaderboard} from "./services/RankService";
 import {ensureUniqueAnswers, reportCurrentVotes, sendAwardQuestions} from './services/CommunityAwardService';
 import {AwardQuestions} from './services/constants/award-questions';
 import {WOMClient} from "@wise-old-man/utils";
+import path from "path";
+import fs from "fs";
 
 dotenv.config();
 let lastRequestForPointsTime: number | null = null;
@@ -54,6 +56,23 @@ const rateLimitSeconds = 1;
             ],
             partials: [Partials.User, Partials.Reaction, Partials.Message, Partials.Channel]
         });
+
+        client.commands = new Collection();
+
+        const commandsPath = path.join(__dirname, 'commands');
+        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+        for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const { command } = require(filePath);
+            // Set a new item in the Collection with the key as the command name and the value as the exported module
+            if ('data' in command && 'execute' in command) {
+                client.commands.set(command.data.name, command);
+            } else {
+                console.log(command);
+                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            }
+        }
 
         await client.login(process.env.TOKEN);
         await connect();
@@ -357,6 +376,24 @@ const rateLimitSeconds = 1;
                         console.log("unable to send server leave message")
                     }
                 }
+            }
+        });
+
+        client.on(Events.InteractionCreate, async interaction => {
+            if (!interaction.isChatInputCommand()) return;
+
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
             }
         });
     } catch (e) {
